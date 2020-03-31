23cdcfbb6d537ddffda9b7eea9f85cc3d0f95fbf
Yegor Trussov <egortr2004@mail.ru>
2020-03-31 12:34:05 +0600

    parallax animation started

--- js/app.js
+++ js/app.js
@@ -1,56 +1,56 @@
-console.log('Hello')
+// console.log('Hello')
 
-let currentSlide = 1;
-let nextSlide = 1;
-const slideNum = 2;
+// let currentSlide = 1;
+// let nextSlide = 1;
+// const slideNum = 2;
 
-document.addEventListener("wheel", throttle(scrollChange, 1500));
+// document.addEventListener("wheel", throttle(scrollChange, 1500));
 
-function init() {
-    for (let i = 2; i <= slideNum; i++) {
-        let slide = document.querySelector(`#section${ i }`);
-        slide.classList.add('hidden');
-    }
-}
+// function init() {
+//     for (let i = 2; i <= slideNum; i++) {
+//         let slide = document.querySelector(`#section${ i }`);
+//         slide.classList.add('hidden');
+//     }
+// }
 
-init();
+// init();
 
-function scrollChange(e) {
-    console.log(e);
-    if (e.deltaY > 0) {
-        if (currentSlide !== slideNum) {
-            nextSlide = currentSlide + 1;
-            changeSlide();
-        }
-    } else {
-        if (currentSlide !== 1) {
-            nextSlide = currentSlide - 1;
-            changeSlide();
-        } 
-    }
-}
+// function scrollChange(e) {
+//     console.log(e);
+//     if (e.deltaY > 0) {
+//         if (currentSlide !== slideNum) {
+//             nextSlide = currentSlide + 1;
+//             changeSlide();
+//         }
+//     } else {
+//         if (currentSlide !== 1) {
+//             nextSlide = currentSlide - 1;
+//             changeSlide();
+//         } 
+//     }
+// }
 
-function changeSlide() {
-    console.log({ currentSlide, nextSlide });
+// function changeSlide() {
+//     console.log({ currentSlide, nextSlide });
 
-    // Do styling stuff to change slides 
-    let curr = document.querySelector(`#section${ currentSlide }`);
-    let next = document.querySelector(`#section${ nextSlide }`);
-    curr.classList.add('hidden');
-    next.classList.remove('hidden');
+//     // Do styling stuff to change slides 
+//     let curr = document.querySelector(`#section${ currentSlide }`);
+//     let next = document.querySelector(`#section${ nextSlide }`);
+//     curr.classList.add('hidden');
+//     next.classList.remove('hidden');
 
-    currentSlide = nextSlide;
-}
+//     currentSlide = nextSlide;
+// }
 
-function throttle(func, limit) {
-    let inThrottle;
-    return function() {
-        const args = arguments;
-        const context = this;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit);
-        }
-    };
-}
+// function throttle(func, limit) {
+//     let inThrottle;
+//     return function() {
+//         const args = arguments;
+//         const context = this;
+//         if (!inThrottle) {
+//             func.apply(context, args);
+//             inThrottle = true;
+//             setTimeout(() => (inThrottle = false), limit);
+//         }
+//     };
+// }
---
+++ js/parallax.js
@@ -0,0 +1,4 @@
+var scene = document.getElementById('scene');
+var scene1 = document.getElementById('scene1');
+var parallaxInstance = new Parallax(scene);
+var parallaxInstance1 = new Parallax(scene1);
